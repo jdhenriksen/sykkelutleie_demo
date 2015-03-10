85229a96cb59882d30f910c328ba04a1e86f3a95
soofstad <stigofstad@gmail.com>
2015-03-10 13:14:35 +0100

    Tracked DBTest/DBUtility

---
+++ sykkelutleie_demo/DBTest.vb
@@ -0,0 +1,27 @@
+﻿Public Class DBTest
+    Dim anySqlQuery As New DBUtility
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        DataGridView1.DataSource = anySqlQuery.selectQuery("SELECT * FROM ansatt")
+
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        Dim celle As Integer
+        TextBox1.Text = DataGridView1.CurrentCell.Value
+        celle = DataGridView1.CurrentCell.ColumnIndex + 1
+
+        TextBox2.Text = DataGridView1.CurrentRow.Cells(celle).Value
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If anySqlQuery.updateQuary("INSERT INTO  `adr05`.`ansatt` (`brukernavn` ,`passord` ,`fornavn` ,`etternavn` ,`telefon` ,`epost` ,`adresse` ,`stilling` ,`postnr` ,`aktivert`)VALUES ('testbrukernavn',  'testpassord',  'Per',  'Olav',  '8888887',  'test@mail.cns',  'testveien 118',  'Lagerarbeider',  '7020',  '1')") = True Then
+            MsgBox("INSERT vellykket")
+        End If
+
+
+
+    End Sub
+End Class
---
+++ sykkelutleie_demo/DBUtility.vb
@@ -0,0 +1,55 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.Configuration
+Public Class DBUtility
+    ''' <summary>
+    ''' Database funksjon som håndterer alle meldinger til DB
+    ''' </summary>
+    ''' <param name="sql"> Skrives som en mySQL setning As String. SELECT, ALTER eller INSERT</param>
+    ''' <returns> Returnerer en DataTable på alle treff</returns>
+    ''' <remarks>ConnectionString "mySql" ligger i App.config for å lettere forandre DB tilkopling</remarks>
+    Public Function selectQuery(sql As String) As DataTable
+        Dim data As New DataTable
+        Dim connection As MySqlConnection = New MySqlConnection
+        connection.ConnectionString = ConfigurationManager.ConnectionStrings("mySql").ConnectionString
+        Try
+            connection.Open()
+            Dim adapter As New MySqlDataAdapter
+            adapter.SelectCommand = New MySqlCommand(sql, connection)
+            adapter.Fill(data)
+            connection.Close()
+
+        Catch ex As MySqlException
+            errorMessage(ex)
+        Finally
+            connection.Dispose()
+        End Try
+
+        Return data
+
+    End Function
+
+    Public Function updateQuary(sql As String) As Boolean
+        Dim connection As MySqlConnection = New MySqlConnection
+        connection.ConnectionString = ConfigurationManager.ConnectionStrings("mySql").ConnectionString
+        Try
+            connection.Open()
+            Dim adapter As New MySqlDataAdapter
+            adapter.UpdateCommand = New MySqlCommand(sql, connection)
+            connection.Close()
+
+        Catch ex As MySqlException
+            errorMessage(ex)
+        Finally
+            connection.Dispose()
+        End Try
+
+        Return True
+
+    End Function
+
+    Private Sub errorMessage(ex As MySqlException)
+        MessageBox.Show("Feil ved oppkobling til database: " & ex.Message)
+
+    End Sub
+End Class
+
