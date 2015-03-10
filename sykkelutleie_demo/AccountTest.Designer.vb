6acca8f2ad1efeca3a1427724bae044f7712ff2a
soofstad <stigofstad@gmail.com>
2015-03-10 12:34:42 +0100

    add updateQuery function

--- sykkelutleie_demo/AccountTest.Designer.vb
+++ sykkelutleie_demo/AccountTest.Designer.vb
@@ -115,7 +115,7 @@ Partial Class AccountTest
         '
         Me.Button1.Location = New System.Drawing.Point(308, 200)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(128, 33)
         Me.Button1.TabIndex = 9
         Me.Button1.Text = "Database Test"
         Me.Button1.UseVisualStyleBackColor = True
--- sykkelutleie_demo/AccountTest.vb
+++ sykkelutleie_demo/AccountTest.vb
@@ -32,6 +32,6 @@
 
     Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Hide()
-        Form1.Show()
+        DBTest.Show()
     End Sub
 End Class
