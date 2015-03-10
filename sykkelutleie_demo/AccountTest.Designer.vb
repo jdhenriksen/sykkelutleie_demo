362727a67291f917ec91ef913fc3cf5d4fc53e44
jdhenriksen <jonasduanehenriksen@gmail.com>
2015-03-10 10:45:19 +0100

    AccountTest Update

---
+++ sykkelutleie_demo/AccountTest.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AccountTest
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnCreateAccount = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnChangeUsername = New System.Windows.Forms.Button()
+        Me.btnChangePassword = New System.Windows.Forms.Button()
+        Me.btnStatus = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(96, 80)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(129, 20)
+        Me.txtUsername.TabIndex = 0
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(96, 106)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(129, 20)
+        Me.txtPassword.TabIndex = 1
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(150, 142)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 2
+        Me.btnLogin.Text = "Logg inn"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'btnCreateAccount
+        '
+        Me.btnCreateAccount.Location = New System.Drawing.Point(247, 29)
+        Me.btnCreateAccount.Name = "btnCreateAccount"
+        Me.btnCreateAccount.Size = New System.Drawing.Size(101, 23)
+        Me.btnCreateAccount.TabIndex = 3
+        Me.btnCreateAccount.Text = "Lag konto"
+        Me.btnCreateAccount.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 80)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Brukernavn"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 109)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Passord"
+        '
+        'btnChangeUsername
+        '
+        Me.btnChangeUsername.Location = New System.Drawing.Point(247, 67)
+        Me.btnChangeUsername.Name = "btnChangeUsername"
+        Me.btnChangeUsername.Size = New System.Drawing.Size(101, 23)
+        Me.btnChangeUsername.TabIndex = 6
+        Me.btnChangeUsername.Text = "Endre brukernavn"
+        Me.btnChangeUsername.UseVisualStyleBackColor = True
+        '
+        'btnChangePassword
+        '
+        Me.btnChangePassword.Location = New System.Drawing.Point(247, 102)
+        Me.btnChangePassword.Name = "btnChangePassword"
+        Me.btnChangePassword.Size = New System.Drawing.Size(101, 23)
+        Me.btnChangePassword.TabIndex = 7
+        Me.btnChangePassword.Text = "Endre passord"
+        Me.btnChangePassword.UseVisualStyleBackColor = True
+        '
+        'btnStatus
+        '
+        Me.btnStatus.Location = New System.Drawing.Point(247, 142)
+        Me.btnStatus.Name = "btnStatus"
+        Me.btnStatus.Size = New System.Drawing.Size(101, 23)
+        Me.btnStatus.TabIndex = 8
+        Me.btnStatus.Text = "Vis status"
+        Me.btnStatus.UseVisualStyleBackColor = True
+        '
+        'AccountTest
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(473, 264)
+        Me.Controls.Add(Me.btnStatus)
+        Me.Controls.Add(Me.btnChangePassword)
+        Me.Controls.Add(Me.btnChangeUsername)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCreateAccount)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Name = "AccountTest"
+        Me.Text = "AccountTest"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
+    Friend WithEvents btnCreateAccount As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnChangeUsername As System.Windows.Forms.Button
+    Friend WithEvents btnChangePassword As System.Windows.Forms.Button
+    Friend WithEvents btnStatus As System.Windows.Forms.Button
+
+End Class
---
+++ sykkelutleie_demo/AccountTest.vb
@@ -0,0 +1,32 @@
+﻿Public Class AccountTest
+    Private a1 As Account
+    Private Sub btnCreateAccount_Click(sender As Object, e As EventArgs) Handles btnCreateAccount.Click
+        a1 = New Account("bruker1", "bruker0001")
+        MsgBox(a1.toString)
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim username = txtUsername.Text
+        Dim password = txtPassword.Text
+        If a1.login(username, password) Then
+            AccountTest2.Show()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnChangeUsername.Click
+        a1.setUsername(InputBox("Nytt brukernavn:"))
+        MsgBox("Brukernavn endret.")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnChangePassword.Click
+        If Not a1.setPassword(InputBox("Nytt passord:")) Then
+            MsgBox("Passord for kort. Prøv igjen.")
+        Else
+            MsgBox("Passord endret.")
+        End If
+    End Sub
+
+    Private Sub btnStatus_Click(sender As Object, e As EventArgs) Handles btnStatus.Click
+        MsgBox(a1.toString)
+    End Sub
+End Class
