f031be42510ee6401b9fdcad568a022e951d1a0d
kaysindre <kaysindre@outlook.com>
2015-04-08 11:55:28 +0200

    BikeModel opprydning

--- sykkelutleie_demo/controller/Bike.vb
+++ sykkelutleie_demo/controller/Bike.vb
@@ -1,9 +1,9 @@
-﻿Public Class Bike
+﻿Public Class Bike 
     Private frameNumber As String
     Private status As String
     Private location As String
     Private placeOfOrigin As String
-    Private Property brakes As String
+    Private brakes As String
     Private tires As String
     Private frame As String
     Private gear As String
@@ -11,21 +11,12 @@
     Dim sqlstring As String
     Dim anySqlQuery As New DBUtility
     Dim myData As New DataTable
-    Dim svar As String
+    Dim answer As String
 
 
     ''' <summary>
     ''' Nytt bikeobject
     ''' </summary>
-    ''' <param name="framenb"></param>
-    ''' <param name="stat"></param>
-    ''' <param name="loc"></param>
-    ''' <param name="placeorigin"></param>
-    ''' <param name="brake"></param>
-    ''' <param name="tire"></param>
-    ''' <param name="framename"></param>
-    ''' <param name="gearname"></param>
-    ''' <param name="modelobject"></param>
     ''' <remarks>Tar i mot modellobjektet i tilegg til alle de andre parametere</remarks>
     Public Sub New(ByVal framenb As String, ByVal stat As String,
                     ByVal loc As String, ByVal placeorigin As String,
@@ -47,21 +38,20 @@
     ''' Ny sykkel
     ''' </summary>
     ''' <remarks>Hvis True altså utført får vi bekreftelse</remarks>
-    Public Sub createBike()
+    Public Function createBike()
 
         sqlstring = "INSERT INTO sykkel (rammenr, status, lokasjon, utleiested, bremser, dekk, ramme, gir, modell) VALUES ('" & frameNumber & "','" & getStatus() & "','" & getLocation() & "','" & getPlaceOfOrigins() & "','" & brakes & "','" & getTires() & "','" & getFrame() & "','" & getGear() & "','" & getModel() & "')"
 
-        svar = anySqlQuery.updateQuery(sqlstring)
-        If svar = "True" Then
-            MsgBox("Sykkel er opprettet")
-        End If
+        answer = anySqlQuery.updateQuery(sqlstring)
 
-    End Sub
+        Return answer
+
+    End Function
     ''' <summary>
     ''' Søk etter sykkel
     ''' </summary>
     ''' <remarks>Fyller datagridview for sykkel med reultater</remarks>
-    Public Function searchBike() As DataTable
+    Public Function searchBike()
 
         myData = anySqlQuery.selectQuery("SELECT rammenr, modell, lokasjon, status FROM sykkel WHERE (rammenr LIKE '%" & frameNumber & "%') AND (modell LIKE '%" & getModel() & "%') AND (status LIKE '" & getStatus() & "%') AND (lokasjon LIKE '%" & getLocation() & "%') AND (utleiested LIKE '%" & getPlaceOfOrigins() & "%') AND (bremser LIKE '%" & brakes & "%') AND (dekk LIKE '%" & getTires() & "%') AND (ramme LIKE '%" & getFrame() & "%') AND (gir LIKE '%" & getGear() & "%') AND (modell LIKE '%" & getModel() & "%')")
 
@@ -69,44 +59,29 @@
 
     End Function
     ''' <summary>
-    ''' Finner sykkel og modelldata 
-    ''' </summary>
-    ''' <returns>Datatabell basert på sql spørring</returns>
-    ''' <remarks>Joiner sykkel og modell for å gi data fra begge</remarks>
-    Public Function searchBicycleModel() As DataTable
-
-        myData = anySqlQuery.selectQuery("SELECT rammenr, kategori, pris, produsent, sykkel.modell FROM sykkel JOIN modell ON sykkel.modell=modell.modell WHERE  (rammenr LIKE '%" & frameNumber & "%') AND (sykkel.modell LIKE '%" & getModel() & "%') AND (status LIKE '%" & getStatus() & "%') AND (lokasjon LIKE '%" & getLocation() & "%') AND (utleiested LIKE '%" & getPlaceOfOrigins() & "%') AND (bremser LIKE '%" & brakes & "%') AND (dekk LIKE '%" & getTires() & "%') AND (ramme LIKE '%" & getFrame() & "%') AND (gir LIKE '%" & getGear() & "%') AND (pris >=" & getPrice() & ") AND (produsent LIKE '%" & getProducer() & "%') AND (kategori LIKE '%" & getCategory() & "%')")
-
-        Return myData
-    End Function
-
-    ''' <summary>
     ''' Endre Sykkel
     ''' </summary>
     ''' <remarks>oppdatering av sykkelinfo, alle bokser og felter oppdateres
     ''' Hvis bekreftet svar= True så gir vi bekreftelse til bruker</remarks>
-    Public Sub changeBike()
+    Public Function changeBike()
 
-        svar = anySqlQuery.updateQuery("UPDATE sykkel SET status ='" & getStatus() & "', lokasjon ='" & getLocation() & "', utleiested='" & getPlaceOfOrigins() & "', bremser='" & brakes & "', dekk= '" & getTires() & "', ramme= '" & getFrame() & "', gir ='" & getGear() & "', modell ='" & getModel() & "' WHERE rammenr ='" & frameNumber & "'")
+        answer = anySqlQuery.updateQuery("UPDATE sykkel SET status ='" & getStatus() & "', lokasjon ='" & getLocation() & "', utleiested='" & getPlaceOfOrigins() & "', bremser='" & brakes & "', dekk= '" & getTires() & "', ramme= '" & getFrame() & "', gir ='" & getGear() & "', modell ='" & getModel() & "' WHERE rammenr ='" & frameNumber & "'")
 
-        If svar = "True" Then
-            MsgBox("Sykkel er oppdatert")
-        End If
+        Return answer
 
-    End Sub
+    End Function
     ''' <summary>
     ''' Slette sykkel
     ''' </summary>
     ''' <remarks>Bruker også objectet, men trenger kun modellnavn
     ''' Returnerer også svar i form av msgbox()</remarks>
-    Public Sub deleteBike()
+    Public Function deleteBike()
 
-        svar = anySqlQuery.updateQuery("DELETE FROM sykkel WHERE rammenr ='" & frameNumber & "'")
-        If svar = "True" Then
-            MsgBox("Sykkel ble slettet")
-        End If
+        answer = anySqlQuery.updateQuery("DELETE FROM sykkel WHERE rammenr ='" & frameNumber & "'")
 
-    End Sub
+        Return answer
+
+    End Function
     Public Function getFrameNumber()
         Return frameNumber
     End Function
@@ -138,8 +113,8 @@
         Else
 
             myData = anySqlQuery.selectQuery("SELECT modell FROM sykkel WHERE rammenr = '" & framenb & "'")
-            svar = myData.Rows(0)(0).ToString()
-            Return svar
+            answer = myData.Rows(0)(0).ToString()
+            Return answer
         End If
 
     End Function
@@ -152,8 +127,8 @@
             Dim modellnavn As String = myData.Rows(0)(0).ToString()
 
             myData = anySqlQuery.selectQuery("SELECT pris FROM modell WHERE modell = '" & modellnavn & "'")
-            svar = myData.Rows(0)(0).ToString()
-            Return svar
+            answer = myData.Rows(0)(0).ToString()
+            Return answer
         End If
 
     End Function
@@ -166,8 +141,8 @@
             Dim modellnavn As String = myData.Rows(0)(0).ToString()
 
             myData = anySqlQuery.selectQuery("SELECT produsent FROM modell WHERE modell = '" & modellnavn & "'")
-            svar = myData.Rows(0)(0).ToString()
-            Return svar
+            answer = myData.Rows(0)(0).ToString()
+            Return answer
         End If
 
     End Function
@@ -180,8 +155,8 @@
             Dim modellnavn As String = myData.Rows(0)(0).ToString()
 
             myData = anySqlQuery.selectQuery("SELECT kategori FROM modell WHERE modell = '" & modellnavn & "'")
-            svar = myData.Rows(0)(0).ToString()
-            Return svar
+            answer = myData.Rows(0)(0).ToString()
+            Return answer
         End If
 
     End Function
